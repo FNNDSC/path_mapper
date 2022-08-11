fcad9295cf827200f4534d0d2cc0402b0cc73aff
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2022-08-11 10:17:24 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='path_mapper',
+    packages=['path_mapper'],
+    description='Utilities for directory tree traversal',
+    version='0.1',
+    url='http://github.com/FNNDSC/path_mapper',
+    author='FNNDSC',
+    author_email='dev@babyMRI.org',
+    keywords=['pip','path','mapper']
+    )
